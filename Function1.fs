39e8c83b66cef1e564703acffe28850e71261ce5
Roger Brogan <roger.brogan@avanade.com>
2018-09-28 13:25:29 -0500

    Added git ignore.

--- Function1.fs
+++ Function1.fs
@@ -7,6 +7,6 @@ open Microsoft.Extensions.Logging
 module FSharpCosmosFunc =
         [<FunctionName("FSharpCosmosFunc")>]
         let Run([<CosmosDBTrigger(databaseName="TestDB", collectionName= "TestCol", ConnectionStringSetting = "LocalCosmosDB",LeaseCollectionName = "leases")>] input: IReadOnlyList<Document> , log: ILogger ) =
-            log.LogInformation("Documents modified " + string input.Count  )
+            log.LogInformation("Documents modified " + string input.Count)
             log.LogInformation("First document Id " + input.[0].Id)
             
